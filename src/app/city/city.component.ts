407b59bf836974a90b0f965eadde013fb9866ecb
alicethomas <alice_thomas@hotmail.com>
2022-06-04 00:18:55 +0300

    metric and imperial constants added

--- src/app/city/city.component.ts
+++ src/app/city/city.component.ts
@@ -9,6 +9,16 @@ export interface weather_details {
   main: string
 }
 
+export interface temperature_details {
+  feels_like: number,
+  temp: number
+}
+export interface units {
+  id: number,
+  name: string,
+  const: string
+}
+
 @Component({
   selector: 'app-city',
   templateUrl: './city.component.html',
@@ -20,13 +30,21 @@ export class CityComponent implements OnInit {
   display_info: boolean = false;
   error_message: boolean = false;
   message: string;
+  selected_temperature_unit: number = 1;
+  temp_unit : units;
+  temperature_units: units[] = [
+    {id: 1, name: 'Metric', const: '°C'},
+    {id: 2, name: 'Imperial', const: '°F'}];
   weather: weather_details = {
                                 description: '',
                                 icon: '',
                                 id: '',
                                 main: ''
                               };
-
+  temperature: temperature_details =  {
+    feels_like: 0,
+    temp: 0
+  }
   constructor(private weatherService: WeatherService) { }
 
   ngOnInit(): void {
@@ -41,13 +59,13 @@ export class CityComponent implements OnInit {
     }
     else {
       this.error_message = false;
-      
-    this.weatherService.getWeather(this.city_name).subscribe((res: any) => {
-      console.log('res', res);
-      this.weather = res.weather[0];
-    });
+      this.temp_unit = this.temperature_units.filter(b => {return b.id == this.selected_temperature_unit})[0];
+      this.weatherService.getWeather(this.city_name, this.temp_unit.name).subscribe((res: any) => {
+        this.weather = res.weather[0];
+        this.temperature = res.main
+      });
 
-    this.displayInfo(true);
+      this.displayInfo(true);
     }
   }
 
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -9,9 +9,9 @@ export class WeatherService {
   private weatherUrl = 'https://api.openweathermap.org/data/2.5/weather';
   constructor(private http: HttpClient) { }
 
-  getWeather(city_name: string)
+  getWeather(city_name: string, temperature_unit: string)
   {
     console.log('came to the service');
-    return this.http.get(this.weatherUrl + "?q="+city_name+"&appid=96b2d8899b1dc93083dbe4b85ed75de6");
+    return this.http.get(this.weatherUrl + "?q="+city_name+"&units="+temperature_unit+"&appid=96b2d8899b1dc93083dbe4b85ed75de6");
   }
 }
