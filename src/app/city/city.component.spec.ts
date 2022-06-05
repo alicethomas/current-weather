a8620899049c344efe390e405aa778789697fdb3
alicethomas <alice_thomas@hotmail.com>
2022-06-06 00:05:18 +0300

    more tests

--- src/app/city/city.component.spec.ts
+++ src/app/city/city.component.spec.ts
@@ -25,8 +25,18 @@ describe('CityComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  fit('should contain Submit button', () => {
+  it('should contain Submit button', () => {
     const btn = fixture.debugElement.nativeElement.querySelector('#fetch_weather');
     expect(btn.innerHTML).toBe('Submit');
   });
+
+  it('should show error message for empty city field', () => {
+    component.city_name = '';
+    component.selected_temperature_unit = 1;
+    component.fetchWeather(component.selected_temperature_unit);
+    fixture.detectChanges();
+    expect(component.message).toEqual('Please enter a city name and try again');
+
+  });
+
 });
