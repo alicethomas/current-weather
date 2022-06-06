b0a2efd834d8ec901498275b3cfec8031b24eafe
alicethomas <alice_thomas@hotmail.com>
2022-06-07 00:18:20 +0300

    Multiple cities shown for the same name

--- src/app/city/city.component.ts
+++ src/app/city/city.component.ts
@@ -1,22 +1,30 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 import { WeatherService } from '../services/weather.service';
 
-export interface weather_details {
+export interface WeatherDetails {
   description: string,
   icon: string,
   id: string,
   main: string
 }
 
-export interface temperature_details {
+export interface TemperatureDetails {
   feels_like: number,
   temp: number
 }
-export interface units {
+
+export interface Units {
   id: number,
   name: string,
   const: string
+}
+
+export interface CityDetails {
+  id: number,
+  city: string,
+  country: string,
+  latitude: number,
+  longitude: number
 }
 
 @Component({
@@ -27,44 +35,56 @@ export interface units {
 export class CityComponent implements OnInit {
 
   city_name: string = '';
+  selectedCity = <CityDetails>{};
+  cities_list: CityDetails[] = [];
+
+
   display_info: boolean = false;
-  error_message: boolean = false;
-  message: string;
+  set_error_message: boolean = false;
+  error_message: string;
+
   selected_temperature_unit: number = 1;
-  temp_unit : units = {id: 1, name: 'Metric', const: '°C'};
-  temperature_units: units[] = [
-    {id: 1, name: 'Metric', const: '°C'},
-    {id: 2, name: 'Imperial', const: '°F'}];
-  weather: weather_details = {
-                                description: '',
-                                icon: '',
-                                id: '',
-                                main: ''
-                              };
-  temperature: temperature_details =  {
-    feels_like: 0,
-    temp: 0
-  }
+  temp_unit : Units = {
+                        id: 1, 
+                        name: 'Metric', 
+                        const: '°C'
+                      };
+  temperature_units: Units[] = [
+                                {id: 1, name: 'Metric', const: '°C'},
+                                {id: 2, name: 'Imperial', const: '°F'}
+                              ];
+  temperature: TemperatureDetails = {
+                                      feels_like: 0,
+                                      temp: 0
+                                    }
+
+  weather: WeatherDetails = {
+                              description: '',
+                              icon: '',
+                              id: '',
+                              main: ''
+                            };
+
   constructor(private weatherService: WeatherService) { }
 
   ngOnInit(): void {
   }
 
-  fetchWeather(temp_unit: number)
+  fetchCityList(temp: number)
   {
+    this.cities_list = [];
     if (this.city_name == '')
     {
-      this.error_message = true;
-      this.message = 'Please enter a city name and try again';
+      this.set_error_message = true;
+      this.error_message = 'Please enter a city name and try again';
     }
     else {
-      this.error_message = false;
-      this.temp_unit = this.temperature_units.filter(b => {return b.id == temp_unit})[0];
-      this.weatherService.getWeather(this.city_name, this.temp_unit.name).subscribe((res: any) => {
-        this.weather = res.weather[0];
-        this.temperature = res.main;
-        this.displayInfo(true);
-
+      this.set_error_message = false;
+      this.temp_unit = this.temperature_units.filter(b => {return b.id == temp})[0];
+      this.weatherService.findCityOptions(this.city_name, this.temp_unit.name).subscribe((res: any) => {
+        res.list.forEach((el: any) => {
+          this.cities_list.push({'id':el.id,'city': el.name,'country': el.sys.country, 'latitude': el.coord.lat, 'longitude': el.coord.lon})
+        });
       },
       error => {
         alert(this.city_name + ' ' + error.statusText);
@@ -73,16 +93,34 @@ export class CityComponent implements OnInit {
     }
   }
 
+  onSelect(city: CityDetails)
+  {
+    this.selectedCity = city;
+    this.fetchCityWeather(city);
+  }
+
+  fetchCityWeather(city: CityDetails)
+  {
+    this.cities_list = [];
+    this.temp_unit = this.temperature_units.filter(b => {return b.id == this.selected_temperature_unit})[0];
+    this.weatherService.getCityWeather(city.latitude, city.longitude, this.temp_unit.name).subscribe((response: any) => {
+      this.city_name = this.selectedCity.city;
+      this.weather = response.current.weather[0];
+      this.temperature = {'feels_like': response.current.feels_like, 'temp': response.current.temp};
+    });
+    this.displayInfo(true);
+  }
+
   showMetricTemperature()
   {
     this.selected_temperature_unit = 1;
-    this.fetchWeather(this.selected_temperature_unit);
+    this.fetchCityWeather(this.selectedCity);
   }
 
   showImperialTemperature()
   {
     this.selected_temperature_unit = 2;
-    this.fetchWeather(this.selected_temperature_unit);
+    this.fetchCityWeather(this.selectedCity);
   }
 
   controlDisplay()
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -6,11 +6,18 @@ import { HttpClient } from '@angular/common/http';
 })
 export class WeatherService {
 
-  private weatherUrl = 'https://api.openweathermap.org/data/2.5/weather';
+  private weatherfindUrl = 'https://api.openweathermap.org/data/2.5/find';
+  private weatherUrl = 'https://api.openweathermap.org/data/2.5/onecall';
   constructor(private http: HttpClient) { }
 
-  getWeather(city_name: string, temperature_unit: string)
+  findCityOptions(city_name: string, temperature_unit: string)
   {
-    return this.http.get(this.weatherUrl + "?q="+city_name+"&units="+temperature_unit+"&appid=96b2d8899b1dc93083dbe4b85ed75de6");
+    return this.http.get(this.weatherfindUrl + "?q="+city_name+"&units="+temperature_unit+"&appid=96b2d8899b1dc93083dbe4b85ed75de6");
   }
+
+  getCityWeather(latitude: number, longitude: number, temperature_unit: string)
+  {
+    return this.http.get(this.weatherUrl + "?lat=" + latitude + "&lon=" + longitude + "&units=" + temperature_unit + "&appid=96b2d8899b1dc93083dbe4b85ed75de6")
+  }
+
 }
