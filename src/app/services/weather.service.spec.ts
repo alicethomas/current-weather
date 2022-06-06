189378098760a7db29218b5bc2bea2bc57dbd3b5
alicethomas <alice_thomas@hotmail.com>
2022-06-06 19:10:17 +0300

    change method name

--- src/app/services/weather.service.spec.ts
+++ src/app/services/weather.service.spec.ts
@@ -1,7 +1,6 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { WeatherService } from './weather.service';
-import { HttpClient } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 
